d9f6d3be61aac4e8f5d9440e6b88c9fccf0d3a5d
vanshot super <vanya12345654@gmail.com>
2023-05-03 20:13:25 +0300

    z

---
+++ Dz6.4/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dz6._4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Sorting sorting = new Sorting(1, 2, 3, 4, 5, 6) ;
+            sorting.Sort();
+            sorting.Add(10);
+            sorting.Add(20);
+            sorting.Sort();
+            Console.ReadLine();
+        }
+    }
+    class Sorting
+    {
+        private int[] ints = new int[0];
+        public Sorting(params int[] yes) {
+            ints = yes;  
+        }
+        public void Sort()
+        {
+            Array.Sort(ints);
+            for (int i = 0; i < ints.Length; i++)
+            {
+                Console.WriteLine(ints[i]);
+            }
+        }
+        public void Add(int value)
+        {
+            int[] newInts = new int[ints.Length + 1];
+            Array.Copy(ints, newInts, ints.Length);
+            newInts[ints.Length] = value;
+            ints = newInts;
+        }
+    }
+}
